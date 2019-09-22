13fd419891d31f217934083ba6264c98cd81c55b
Tahnee Hall <tnhall2012@hotmai.com>
2019-09-22 01:40:57 -0400

    added html, css and firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+console.log("I'm a goddess")
+
+/*// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA8D0xL8DLPWJo-hoBdaNcMNZm11026RUA",
+    authDomain: "trainscheduler-8b89a.firebaseapp.com",
+    databaseURL: "https://trainscheduler-8b89a.firebaseio.com",
+    projectId: "trainscheduler-8b89a",
+    storageBucket: "",
+    messagingSenderId: "827410912174",
+    appId: "1:827410912174:web:66040fd5a0905a5c696e3f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);*/
+
+  
